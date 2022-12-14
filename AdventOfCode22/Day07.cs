4e536a947d4ae4bd5daa0423198e30887be65508
Sofie Bäverstrand <70134415+baverstrand@users.noreply.github.com>
2022-12-14 13:52:43 +0100

    update values start

--- AdventOfCode22/Day07.cs
+++ AdventOfCode22/Day07.cs
@@ -34,20 +34,64 @@ namespace AdventOfCode22
                 }
                 else if (char.IsDigit(line[0])) 
                 {
-                    directories = HandleFile(directories, line);
-                    
+                    var o = HandleFile(directories, files, line);
+                    directories.Clear();
+                    directories = o.Directories;
+                    files.Clear();
+                    files= o.Files;
                 }
                 else if (line.StartsWith("dir "))
                 {
-                    // create directory
-                    // set current as parent
-                    // add this to children
+                    directories = HandleDir(directories, line);
                 }
             }
-            
         }
 
-        private static List<D07Directory> HandleFile(List<D07Directory> directories, string line)
+        private static List<D07Directory> HandleDir(List<D07Directory> directories, string line)
+        {
+            var splitLines = line.Split(' ');
+            var currentDirIndex = -1;
+            var isInList = false;
+            for (var i = 0; i < directories.Count; i++)
+            {
+                if (directories[i].Name == splitLines[1])
+                {
+                    isInList = true;
+                }
+                else if (directories[i].IsCurrent)
+                {
+                    currentDirIndex = i;
+                }
+            }
+            if (!isInList)
+            {
+                var d = new D07Directory
+                {
+                    Name = splitLines[1],
+                    Parent = directories[currentDirIndex].Name,
+                    IsCurrent = false,
+                };
+
+                if (directories[currentDirIndex].ChildrenDirs != null &&
+                !directories[currentDirIndex].ChildrenDirs.Contains(splitLines[1]))
+                {
+                    directories[currentDirIndex].ChildrenDirs.Add(splitLines[1]);
+                }
+                else if (directories[currentDirIndex].ChildrenDirs == null)
+                {
+                    List<string> l = new()
+                    {
+                        splitLines[1]
+                    };
+                    directories[currentDirIndex].ChildrenDirs = l;
+                }
+                directories.Add(d);
+                
+            }
+            throw new NotImplementedException();
+        }
+
+        private static D07Obj HandleFile(List<D07Directory> directories, List<D07File> files, string line)
         {
             var splitLine = line.Split(' ');
             var fileSize = double.Parse(splitLine[0]);
@@ -69,6 +113,13 @@ namespace AdventOfCode22
                 FileSize = fileSize
             };
 
+            if (files != null && !files.Contains(f))
+            {
+                files.Add(f);
+                directories = UpdateValues(directories, f);
+            }
+            
+
             if (directories[currentDir].ChildrenFiles != null && 
                 !directories[currentDir].ChildrenFiles.Contains(fileName))
             {
@@ -82,8 +133,37 @@ namespace AdventOfCode22
                 };
                 directories[currentDir].ChildrenFiles = l;
             }
-         
-            throw new NotImplementedException();
+            var o = new D07Obj
+            {
+                Directories = directories,
+                Files = files
+            };
+            return o;
+        }
+
+        private static List<D07Directory> UpdateValues(List<D07Directory> directories, D07File f)
+        {
+            var topLevel = false;
+            var dirName = f.Directory;
+            var dirIndex = -1;
+
+            for (var i = 0; i < directories.Count; i++)
+            {
+                if (directories[i].Name.Equals(dirName))
+                {
+                    dirIndex = i;
+
+                    // TODO Här är du :)  
+                }
+
+            }
+
+            while (!topLevel)
+            {
+                
+            }
+
+            return directories;
         }
 
         private static List<D07Directory> HandleCD(List<D07Directory> directories, string line)
@@ -131,8 +211,13 @@ namespace AdventOfCode22
                     {
                         Name = dir,
                         IsCurrent = true,
-                        Parent = directories[parentIndex].Name
+                        
                     };
+
+                    if (d.Name != "/")
+                    {
+                        d.Parent = directories[parentIndex].Name;
+                    }
 
                     if (directories[parentIndex].ChildrenDirs != null && 
                         !directories[parentIndex].ChildrenDirs.Contains(dir))
---
+++ AdventOfCode22/Models/D07Obj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode22.Models
+{
+    public class D07Obj
+    {
+        public List<D07Directory> Directories { get; set; }
+        public List<D07File> Files { get; set; }
+    }
+}
