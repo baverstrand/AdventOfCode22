0e8bbdda878aa0e22b2b267f13484191af5a07ed
Sofie Bäverstrand <70134415+baverstrand@users.noreply.github.com>
2022-12-21 15:19:13 +0100

    day08 read lines and setup start

--- AdventOfCode22/Day08.cs
+++ AdventOfCode22/Day08.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AdventOfCode22.Help;
+using AdventOfCode22.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,34 @@ namespace AdventOfCode22
     {
         public static void Run()
         {
+           // var day = "08Data";
+            var day = "08Test";
+            var data = Helpers.ReadLines(day);
 
+            // create forest
+            // X = value, Y = isVisible
+            List<List<Point>> forest = new();
+            for (var y = 0; y < data.Length; y++)
+            {
+                List<Point> trees = new();
+                for (var x = 0; x < data[y].Length; x++)
+                {
+                    var tree = new Point
+                    {
+                        X = int.Parse(data[y][x].ToString()),
+                        Y = 0
+                    };
+                    trees.Add(tree);
+                }
+                forest.Add(trees);
+            }
+
+            for (var y = 0; y < forest.Count; y++)
+            {
+                var top = forest[y][0].X;
+                // om y är noll eller sista alla X= 1, nästa rad
+                // om x är noll eller sista X = 1
+            }
         }
     }
 }
---
+++ AdventOfCode22/Models/D08Tree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode22.Models
+{
+    public class D08Tree
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
